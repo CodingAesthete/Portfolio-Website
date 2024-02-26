12d2d7903c718d1a625e0e827e46853bac1561bd
CodingAesthete <harry_olli@abv.bg>
2024-02-26 11:24:15 +0200

    Add alt prop to Image

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -5,7 +5,7 @@ export default function AboutSection() {
   return (
     <section className='text-white' id="about">
       <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        <Image src="/images/about2.png" width={500} height={500} />
+        <Image src="/images/about2.png" width={500} height={500} alt="" />
       </div>
     </section>
   )
