afbde67b7a550e65008f0c9da02c7100c16ddf6d
CodingAesthete <harry_olli@abv.bg>
2024-02-26 11:38:52 +0200

    Add Speed Insights

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
+      <SpeedInsights />
       <Navbar />
       <div className="container mt-6 mx-auto px-12 py-6">
         <HeroSection />
